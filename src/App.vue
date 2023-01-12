5f581d368617787d4d1740c2114fd03f61def5fe
Adit <aditya.umakanth@gmail.com>
2023-01-12 00:01:17 -0600

    Added firebase and OAuth login (#3)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import { ref } from 'vue';
+import { RouterView } from "vue-router";
+import { ref } from "vue";
+import { useQuasar } from "quasar";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithRedirect,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+const $q = useQuasar();
+$q.loading.show();
+
 const leftDrawerOpen = ref(false);
 function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+  leftDrawerOpen.value = !leftDrawerOpen.value;
 }
-</script>
 
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log(user);
+    $q.loading.hide();
+  } else {
+    signInWithRedirect(auth, new GoogleAuthProvider());
+  }
+});
+</script>
 
 <template>
   <q-layout view="lHh lpR lFf">
-
     <q-header reveal elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" class="lt-md" />
+        <q-btn
+          dense
+          flat
+          round
+          icon="menu"
+          @click="toggleLeftDrawer"
+          class="lt-md"
+        />
       </q-toolbar>
       <q-toolbar inset></q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered :width=400>
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      bordered
+      :width="400"
+    >
       <q-toolbar>
-        <q-spacer />
         <q-toolbar-title class="absolute-center q-mt-lg">
           <q-icon name="o_book" size="1.5em" />
           Encrypted Journal
@@ -31,10 +62,7 @@ function toggleLeftDrawer() {
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
-import { Quasar } from "quasar";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase/conf";
+
+import { Quasar, Loading } from "quasar";
 import "@quasar/extras/roboto-font/roboto-font.css";
 import "@quasar/extras/material-icons/material-icons.css";
 import "@quasar/extras/material-icons-outlined/material-icons-outlined.css";
@@ -10,10 +13,16 @@ import "quasar/src/css/index.sass";
 import App from "./App.vue";
 import router from "./router";
 
+console.log(initializeApp(firebaseConfig));
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(Quasar);
+app.use(Quasar, {
+  plugins: {
+    Loading,
+  },
+});
 
 app.mount("#app");
