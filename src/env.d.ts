b32a3dee7a0abccc0d43e6dc4b60979a10978563
Adit Umakanth <aditya.umakanth@gmail.com>
2023-01-19 18:59:11 -0800

    Added commit ID in sidebar

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,6 +7,7 @@ interface ImportMetaEnv {
   readonly VITE_FB_STORAGE_BUCKET: string;
   readonly VITE_FB_SENDER_ID: string;
   readonly VITE_FB_APP_ID: string;
+  readonly VITE_GITHUB_SHA: string;
 }
 
 interface ImportMeta {
