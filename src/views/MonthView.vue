4f494a853aa25b278c93970ef1b7853bf41f28da
Adit Umakanth <aditya.umakanth@gmail.com>
2023-04-22 18:48:05 -0500

    added table for monthly entries

--- src/views/MonthView.vue
+++ src/views/MonthView.vue
@@ -39,6 +39,30 @@ function queryJournalEntries() {
   entries = useCollection(q, { ssrKey: "date" });
 }
 queryJournalEntries();
+
+const columns = [
+  {
+    name: "date",
+    required: true,
+    align: "left",
+    label: "Date",
+  },
+  {
+    name: "section_name",
+    align: "left",
+    label: "Sections",
+  },
+  {
+    name: "title",
+    align: "left",
+    label: "Title",
+  },
+  {
+    name: "rating",
+    align: "left",
+    label: "Rating",
+  },
+];
 </script>
 
 <template>
@@ -49,7 +73,38 @@ queryJournalEntries();
     />
   </div>
   <div class="q-ma-lg">
-    {{ entries }}
-    <!-- <JournalEntryView date="" /> -->
+    <q-table title="Entries" :rows="entries" :columns="columns" row-key="date">
+      <template v-slot:body="props">
+        <q-tr :props="props" @click="router.push(`${props.row.date}`)">
+          <q-td key="date" :props="props">
+            {{ props.row.date }}
+          </q-td>
+          <q-td :props="props" key="date">
+            <ul style="list-style-type: none; padding: 0; margin: 0">
+              <li
+                v-for="section in props.row.sections"
+                :key="section.section_name"
+              >
+                {{ section.section_name }}
+              </li>
+            </ul>
+          </q-td>
+          <q-td :props="props" key="date">
+            <ul style="list-style-type: ''; padding: 0; margin: 0">
+              <li v-for="section in props.row.sections" :key="section.title">
+                {{ section.title }}
+              </li>
+            </ul>
+          </q-td>
+          <q-td :props="props" key="date">
+            <ul style="list-style-type: ''; padding: 0; margin: 0">
+              <li v-for="section in props.row.sections" :key="section.rating">
+                {{ section.rating }}
+              </li>
+            </ul>
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
   </div>
 </template>
